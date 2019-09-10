5b5a8c53fa685036364d12393e604584b8d06a9c
yaopu <1417511526@qq.com>
2019-09-10 08:41:26 +0800

    oqo-01-02

---
+++ memory/type_and_size.cpp
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+	printf("sizeof(int) = %d\n",sizeof(int)); 
+	printf("sizeof(float)=%d\n",sizeof(float)); 
+	printf("sizeof(char)=%d\n",sizeof(char));
+//string
+	printf("sizeof(double)=%d\n",sizeof(double));
+	printf("short int =%d\n",sizeof(short int));
+	printf("long double=%d\n",sizeof(long double));
+	printf("usigned int =%d\n",sizeof(unsigned int));
+}
+
