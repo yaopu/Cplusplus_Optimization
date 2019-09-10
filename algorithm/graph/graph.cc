7c2b9de2242bf67893bf1fe164f71b121f6ef159
yaopu <1417511526@qq.com>
2019-09-10 15:40:10 +0800

    graph

--- algorithm/graph/graph.cc
+++ algorithm/graph/graph.cc
@@ -1,6 +1,11 @@
 // 8个顶点连通无向图的边，有多少个？
+//图的搜素
 //深度优先遍历
 //广度优先遍历
+//图的存储
+//连接矩阵
+//顶点的度数
+/// 深度优先算法的实现https://blog.csdn.net/fanyun_01/article/details/82864580
 void create_graph();
 void search_graph();
 void sear_graph();
